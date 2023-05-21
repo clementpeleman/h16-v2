912b0326a1ec4dbf1d7adf279ccd6ec33b0a3d81
Clement Peleman <clementpeleman@outlook.com>
2023-05-21 16:25:59 +0200

    redirect login

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/login',
+        destination: 'https://dolphin-app-4lkcd.ondigitalocean.app/admin',
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
