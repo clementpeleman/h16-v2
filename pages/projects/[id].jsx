29c5f403da842f975f4f529ef4a926fe7bfd78ac
Clement <clementpeleman@outlook.com>
2023-10-10 12:58:53 +0200

    modified:   pages/projects/[id].jsx

--- pages/projects/[id].jsx
+++ pages/projects/[id].jsx
@@ -93,22 +93,22 @@ function ProjectSingle(props) {
           </div>
 
           {/* Single project social sharing */}
-          {/* <div>
-						<p className="font-general-regular text-2xl font-semibold text-ternary-dark dark:text-ternary-light mb-2">
-							Share This
-						</p>
-						<div className="flex items-center gap-3 mt-5">
-									<Link
-										key={props.project.id}
-										href={props.project.attributes.externe_link}
-										target="__blank"
-										passHref={true}
-										aria-label="Share Project"
-										className="bg-ternary-light dark:bg-ternary-dark text-gray-400 hover:text-primary-dark dark:hover:text-primary-light p-2 rounded-lg shadow-sm duration-500"
-									>
-									</Link>;
-						</div>
-					</div> */}
+          <div>
+            <p className="font-general-regular text-2xl font-semibold text-ternary-dark dark:text-ternary-light mb-2">
+              Share This
+            </p>
+            <div className="flex items-center gap-3 mt-5">
+              <Link
+                key={props.project.id}
+                href={props.project.attributes.externe_link}
+                target="__blank"
+                passHref={true}
+                aria-label="Share Project"
+                className="bg-ternary-light dark:bg-ternary-dark text-gray-400 hover:text-primary-dark dark:hover:text-primary-light p-2 rounded-lg shadow-sm duration-500"
+              ></Link>
+              ;
+            </div>
+          </div>
         </div>
 
         {/*  Single project right section details */}
