25a2ddba99528839ee9fb797865584def23089c8
Clement <clementpeleman@outlook.com>
2023-10-12 18:04:08 +0200

    modified:   components/about/AboutClientSingle.jsx

--- components/about/AboutClientSingle.jsx
+++ components/about/AboutClientSingle.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function AboutClientSingle({ title, image }) {
   return (
-    <div className="py-5 px-10 border h-48 sm:h-24 md:h-32 lg:h-64 bg-secondary-light border-ternary-light dark:border-ternary-dark flex justify-center grayscale  shadow-sm rounded-lg mb-5 cursor-pointer">
+    <div className="py-5 px-10 border h-48 sm:h-24 md:h-32 relative lg:h-64 bg-secondary-light border-ternary-light dark:border-ternary-dark flex justify-center grayscale  shadow-sm rounded-lg mb-5 cursor-pointer">
       <Image
         src={image}
         fill={true}
