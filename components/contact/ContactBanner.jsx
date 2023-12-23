8ed114d6351690248f780104baa5883016efed6d
Clement Peleman <clementpeleman@outlook.com>
2023-12-23 12:58:53 +0100

    small changes

--- components/contact/ContactBanner.jsx
+++ components/contact/ContactBanner.jsx
@@ -4,9 +4,9 @@ export class ContactBanner extends Component {
   render() {
     return (
       <div className="mx-4 sm:mx-0">
-        <section className="pt-5 sm:pt-10 mt-6 sm:mt-8">
+        <section className="lg:pt-5 mt-6 ">
           <div className="">
-            <p className=" font-general-medium text-2xl sm:text-4xl text-black dark:text-ternary-light ">
+            <p className="hidden lg:block font-general-medium text-2xl sm:text-4xl text-black dark:text-ternary-light ">
               Contacteer ons
             </p>
           </div>
