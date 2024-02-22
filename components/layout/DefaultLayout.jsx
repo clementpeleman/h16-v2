5380edfd64b4daae4c998f46e1b67211d2ffa098
clementpeleman <clementpeleman@outlook.com>
2024-02-22 09:13:49 +0100

    modified:   components/layout/DefaultLayout.jsx 	modified:   pages/_app.jsx

--- components/layout/DefaultLayout.jsx
+++ components/layout/DefaultLayout.jsx
@@ -1,7 +1,6 @@
 import AppHeader from "../shared/AppHeader";
 import AppFooter from "../shared/AppFooter";
 import PagesMetaHead from "../PagesMetaHead";
-import { Analytics } from "@vercel/analytics/react";
 
 const DefaultLayout = ({ children }) => {
   return (
@@ -9,7 +8,6 @@ const DefaultLayout = ({ children }) => {
       <PagesMetaHead />
       <AppHeader />
       <div>{children}</div>
-      <Analytics />
       <AppFooter />
     </>
   );
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,19 +1,21 @@
-import '../styles/globals.css';
-import { AnimatePresence } from 'framer-motion';
-import DefaultLayout from '../components/layout/DefaultLayout';
-import UseScrollToTop from '../hooks/useScrollToTop';
+import "../styles/globals.css";
+import { AnimatePresence } from "framer-motion";
+import DefaultLayout from "../components/layout/DefaultLayout";
+import UseScrollToTop from "../hooks/useScrollToTop";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<AnimatePresence>
-			<div className=" bg-background bg-opacity-60 dark:bg-primary-dark transition duration-300">
-				<DefaultLayout>
-					<Component {...pageProps} />
-				</DefaultLayout>
-				<UseScrollToTop />
-			</div>
-		</AnimatePresence>
-	);
+  return (
+    <AnimatePresence>
+      <div className=" bg-background bg-opacity-60 dark:bg-primary-dark transition duration-300">
+        <DefaultLayout>
+          <Component {...pageProps} />
+          <Analytics />
+        </DefaultLayout>
+        <UseScrollToTop />
+      </div>
+    </AnimatePresence>
+  );
 }
 
 export default MyApp;
