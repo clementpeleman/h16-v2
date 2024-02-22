728b2ec90ec394c35cc6c7a0737a6347072deee4
clementpeleman <clementpeleman@outlook.com>
2024-02-22 09:10:26 +0100

    modified:   components/layout/DefaultLayout.jsx 	modified:   package-lock.json 	modified:   package.json

--- components/layout/DefaultLayout.jsx
+++ components/layout/DefaultLayout.jsx
@@ -1,16 +1,18 @@
-import AppHeader from '../shared/AppHeader';
-import AppFooter from '../shared/AppFooter';
-import PagesMetaHead from '../PagesMetaHead';
+import AppHeader from "../shared/AppHeader";
+import AppFooter from "../shared/AppFooter";
+import PagesMetaHead from "../PagesMetaHead";
+import { Analytics } from "@vercel/analytics/react";
 
 const DefaultLayout = ({ children }) => {
-	return (
-		<>
-			<PagesMetaHead />
-			<AppHeader />
-			<div>{children}</div>
-			<AppFooter />
-		</>
-	);
+  return (
+    <>
+      <PagesMetaHead />
+      <AppHeader />
+      <div>{children}</div>
+      <Analytics />
+      <AppFooter />
+    </>
+  );
 };
 
 export default DefaultLayout;
